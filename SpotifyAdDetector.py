288d59953478412ebc335fdd5c8d344c8980f3bb
nikhi1g <nikhilgupta@dpengineering.org>
2021-12-28 18:39:32 -0800

    less breaks, Spotify won't creash, stil red issue

--- SpotifyAdDetector.py
+++ SpotifyAdDetector.py
@@ -9,56 +9,53 @@ import keyboard
 import pyautogui as p
 from threading import Thread
 from subprocess import call
-from time import sleep
-import pyperclip as pyp
+# from time import sleep
+# import pyperclip as pyp
+
+#link below
 # https://developer.spotify.com/console/get-user-player/?market=&additional_types=
 
 SPOTIFY_GET_CURRENT_TRACK_URL = 'https://api.spotify.com/v1/me/player/currently-playing'
-K = "Gonna Gonna Get It"
-
-
-
-
-
-
+K = "Gonna Gonna Get My Key"#enter key here
 ACCESS_TOKEN = K
 
 t = True
 f = False
 keyon = t
 
+SPOTIFYisON = False
 
+# def onstartup():
+#     print('Get_Key')
+#     print(K)
+#     os.system("open https://developer.spotify.com/console/get-user-player/?market=&additional_types=")
+#     sleep(1.5)
+#     p.moveTo(733,834,0.2)
+#     p.click()
+#     p.moveTo(463, 453,0.2)
+#     p.click()
+#     sleep(0.3)
+#     p.scroll(-30)
+#     p.moveTo(727, 829, 0.5)
+#     p.click()
+#     sleep(0.4)
+#     p.moveTo(353, 829, 0.5)
+#     p.mouseDown()
+#     p.moveTo(860, 829, 0.5)
+#     p.hotkey('command','x')
+#     sleep(0.4)
+#     p.hotkey('command', 'w')
+#     sleep(0.5)
+#     os.system("open -a PyCharm")
+#     sleep(0.5)
+#     s = pyp.paste()
+#     global ACCESS_TOKEN
+#     ACCESS_TOKEN = s
+#     print("Get_Key_Complete","\n" + s)
+#     sleep(1)
+#     #KEY = input("Just Blank or Find Key: "), dont use this as it will just lead to wait errors
 
-if keyon:
-    print('Get_Key')
-    print(K)
-    os.system("open https://developer.spotify.com/console/get-user-player/?market=&additional_types=")
-    sleep(0.5)
-    p.moveTo(733,834,0.2)
-    p.click()
-    p.moveTo(463, 453,0.2)
-    p.click()
-    sleep(0.3)
-    p.scroll(-30)
-    p.moveTo(727, 829, 0.5)
-    p.click()
-    sleep(0.4)
-    p.moveTo(353, 829, 0.5)
-    p.mouseDown()
-    p.moveTo(860, 829, 0.5)
-    p.hotkey('command','c')
-    p.hotkey('command', 'w')
-    sleep(0.5)
-    os.system("open -a PyCharm")
-    sleep(0.5)
-    s = pyp.paste()
-    ACCESS_TOKEN = s
-    print("Get_Key_Complete","\n" + s)
-    sleep(10)
-    #KEY = input("Just Blank or Find Key: "), dont use this as it will just lead to wait errors
-
-#don't abuse this too much or you will get ratelimited
-
+#don't abuse this too much or you will get ratelimited ;( or token limited, not sure which
 def exit():
     os.system("pkill Spotify")
     # subprocess.call(['osascript', '-e', 'tell application "System Events" to sleep']) #useful sleep function
@@ -84,6 +81,7 @@ def main():
         except Exception as e:
             print(e, 'in main')
             main()
+
             # break
 
         time.sleep(1)
@@ -112,20 +110,23 @@ def get_current_track(access_token):
 
         current_track_info = {
             "id": track_id,
-            "track_name": track_name,
-            "artists": artist_names,
-            "link": link
+            "track_name": track_name
+            # "artists": artist_names,
+            # "link": link
         }
 
         return current_track_info
 
     except Exception as e:
         print(e, 'in get_current_track')
+        # global SPOTIFYisON
+        # SPOTIFYisON = False
         close()
         open()
         play()
         time.sleep(0.4)#idk, maybe this will work. something with active focus?
         minimize()
+        # SPOTIFYisON = True
         time.sleep(
             5)  # 5 seconds before main? maybe this will fix the error that Spotify notifies with, "The Application Spotify Quit Unexpectedly"
         main()
@@ -138,7 +139,12 @@ def close():
 
 def open():
     os.system("open -a Spotify")
-    time.sleep(0.2)
+    # while SPOTIFYisON == False:
+    #     try:
+    #         os.system("open -a Spotify")
+    #     except Exception as e:
+    #         print(e,"in Spotify open")
+    #     time.sleep(0.2)
 
 
 def play():
@@ -182,4 +188,7 @@ def minimize():
 
 
 if __name__ == '__main__':
+    # onstartup()
     main()
+#HTTPSConnectionPool(host='api.spotify.com', port=443): Max retries exceeded with url: /v1/me/player/currently-playing (Caused by NewConnectionError('<urllib3.connection.HTTPSConnection object at 0x11fd28520>: Failed to establish a new connection: [Errno 60] Operation timed out')) in get_current_track
+#what?&&^^
